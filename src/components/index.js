55a04f0291786179cf06cd2e5795364f909b2909
Greta Simonyan <gsimonyan228@gmail.com>
2019-11-26 18:18:29 +0400

    26 11 commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,11 @@
-import Header from '../../components/Header';
-import Carousel from '../../components/Carousel';
-import AboutUs from '../../components/AboutUs';
-import Collections from '../../components/Collections';
-import NewColl from '../../components/NewColl';
-import Gallery from '../../components/Gallery';
-import Info from '../../components/Info';
-import Footer from '../../components/Footer';
+import Header from '../components/Header';
+import Carousel from '../components/Carousel';
+import AboutUs from '../components/AboutUs';
+import Collections from '../components/Collections';
+import NewColl from '../components/NewColl';
+import Gallery from '../components/Gallery';
+import Info from '../components/Info';
+import Footer from '../components/Footer';
 
 export{
     Header,
