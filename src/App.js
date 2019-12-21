581393152e79d1174815049d8d23b1b879490ce1
Greta Simonyan <gsimonyan228@gmail.com>
2019-12-21 13:45:16 +0400

    snow

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Home from './pages/Home';
 
-class App extends React.Component{
-    render(){
-        return(
-            <>
-                <Home/>
-            </>
-        )
-    }
+function App(){
+    return(
+        <>
+            <Home/>
+        </>
+    )
 }
 
 export default App;
