5ba28a587582441a41998c51de1d26e74d40fd14
Greta Simonyan <gsimonyan228@gmail.com>
2019-12-30 16:12:36 +0400

    Updates

---
+++ precache-manifest.0a31cbed459b48ded1c096bb0085a4d3.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebcef2d56583fe7254f3da3a08b28cc5",
+    "url": "/agapess/index.html"
+  },
+  {
+    "revision": "2859f1380d66ce30234e",
+    "url": "/agapess/static/css/2.588d4d09.chunk.css"
+  },
+  {
+    "revision": "0ec232a266dcee5bca48",
+    "url": "/agapess/static/css/main.2ad3bc24.chunk.css"
+  },
+  {
+    "revision": "2859f1380d66ce30234e",
+    "url": "/agapess/static/js/2.aafbfa89.chunk.js"
+  },
+  {
+    "revision": "0ec232a266dcee5bca48",
+    "url": "/agapess/static/js/main.08c36633.chunk.js"
+  },
+  {
+    "revision": "1c3012d7090b67912195",
+    "url": "/agapess/static/js/runtime-main.1b5d4f38.js"
+  },
+  {
+    "revision": "b9b64ffe2fccaba602bc1cbdea54fa2f",
+    "url": "/agapess/static/media/big_snow.b9b64ffe.png"
+  },
+  {
+    "revision": "ddbbba1ef58508be43fd8f1b5204257e",
+    "url": "/agapess/static/media/byAstghik.ddbbba1e.png"
+  },
+  {
+    "revision": "6727bf006241042c5684742e3ec8aea7",
+    "url": "/agapess/static/media/col1.6727bf00.jpg"
+  },
+  {
+    "revision": "b90b32b3b7511feb11436e69bb5e7ed6",
+    "url": "/agapess/static/media/col10.b90b32b3.jpg"
+  },
+  {
+    "revision": "2cf09fae03775d0599fe82c793a2248b",
+    "url": "/agapess/static/media/col2.2cf09fae.jpg"
+  },
+  {
+    "revision": "ccf1e6e7baa721b9d7f14250a5ba3d79",
+    "url": "/agapess/static/media/col3.ccf1e6e7.jpg"
+  },
+  {
+    "revision": "1b9ae3d13c9fe671dbe69511ee6346d0",
+    "url": "/agapess/static/media/col4.1b9ae3d1.jpg"
+  },
+  {
+    "revision": "52d7b8b376b92e56a6cff891a4686864",
+    "url": "/agapess/static/media/col5.52d7b8b3.jpg"
+  },
+  {
+    "revision": "8ad077c81729c3d55c42d5a1e6aaa072",
+    "url": "/agapess/static/media/col6.8ad077c8.jpg"
+  },
+  {
+    "revision": "f33e7564e97edc464104930595460caf",
+    "url": "/agapess/static/media/col7.f33e7564.jpg"
+  },
+  {
+    "revision": "67b05cded60ba31014a71b127ff18e01",
+    "url": "/agapess/static/media/col8.67b05cde.jpg"
+  },
+  {
+    "revision": "055a51d1efaf836a0620006fabdac6b0",
+    "url": "/agapess/static/media/col9.055a51d1.jpg"
+  },
+  {
+    "revision": "b93488253c17b5f68f62b2c1e4b0975f",
+    "url": "/agapess/static/media/cover1.b9348825.jpg"
+  },
+  {
+    "revision": "c264a8506d91cd4404ad74f8f7c67870",
+    "url": "/agapess/static/media/cover2.c264a850.jpg"
+  },
+  {
+    "revision": "6f7b94e7044aa496135881df2df06406",
+    "url": "/agapess/static/media/cover3.6f7b94e7.jpg"
+  },
+  {
+    "revision": "d835e3d4ebf211b8b3bd04f9faa171cb",
+    "url": "/agapess/static/media/logoWhite.d835e3d4.png"
+  },
+  {
+    "revision": "d13f83f938003c5569660b3044fd4115",
+    "url": "/agapess/static/media/newcollection.d13f83f9.jpg"
+  },
+  {
+    "revision": "b0cd588df110f7ced7f90b0db40e3682",
+    "url": "/agapess/static/media/photo1.b0cd588d.jpg"
+  },
+  {
+    "revision": "178eada3c2b06353eb4db00c2de1d46d",
+    "url": "/agapess/static/media/red.178eada3.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/agapess/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/agapess/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/agapess/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/agapess/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b5e550f73aa370702de8cfd86d751522",
+    "url": "/agapess/static/media/snow.b5e550f7.png"
+  },
+  {
+    "revision": "791c17187972f9da21bea039c7ea12d9",
+    "url": "/agapess/static/media/snow_b.791c1718.png"
+  },
+  {
+    "revision": "c966c6c3065d72a766bc96440b0cbcbd",
+    "url": "/agapess/static/media/tel.c966c6c3.jpg"
+  }
+]);
