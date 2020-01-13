9802f8e7e60113b20c065d811534ede6b00eb23f
Greta Simonyan <gsimonyan228@gmail.com>
2020-01-13 12:06:02 +0400

    home

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -14,7 +14,7 @@ const Logo = styled.img`
 `;
 const SocialLink = styled.a`
     text-decoration: none;
-    ${ (props) => props.headerType==2 ? `color: white` : `color: gray` }
+    ${ (props) => props.headerType == 2 ? `color: white` : `color: gray` }
 `;
 const Fb = styled(TiSocialFacebookCircular)`
     font-size: 30px;
@@ -27,7 +27,7 @@ const Link = styled.a`
     color: gray;
     text-transform: uppercase;
     text-decoration: none;
-    ${ (props) => props.headerType==2 && `color: white` }
+    ${ (props) => props.headerType == 2 && `color: white` }
 `;
 
 class Header extends React.Component{
@@ -56,12 +56,15 @@ class Header extends React.Component{
             }
         });
     }
+    scrollHome = () => {
+        window.scrollTo(0,0)
+    }
     render(){
         const { headerType } = this.state
         return(
             <View tr='0.4s' flex z='99' boxS='border-box' pos='fixed' alignI='center' justC='center' m="0 auto" w='100%' p="15px 0" bgColor={this.state.bgcolor} boxSh='0px 11px 32px -9px rgba(0, 0, 0, 0.4)'>
                 <View w='80%' flex justC='space-between'>
-                    <View><NavLink to='/'><Logo src={ headerType===2 ? logoWhite : logoGray }/></NavLink></View>
+                    <View><NavLink to='/'><Logo onClick={this.scrollHome} src={ headerType === 2 ? logoWhite : logoGray }/></NavLink></View>
                     <View flex alignI='center'>
                         <View><Link headerType={headerType} href='#about'>About</Link></View>
                         <View><Link headerType={headerType} href='#collections'>Collections</Link></View>
