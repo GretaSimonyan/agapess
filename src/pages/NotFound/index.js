9c2543c82ddb0aac43f45981e1178d05cb371a06
Greta Simonyan <gsimonyan228@gmail.com>
2020-01-14 17:58:43 +0400

    nfound

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import {
+    View
+} from '../../styled';
+import { NavLink } from 'react-router-dom';
 
 function NotFound(){
     return(
-        <h1>Page Not Found</h1>
+        <View flex justC='center' alignI='center' fD='column'>
+            <View><h1>Page Not Found</h1></View>
+            <View>Please keep calm and </View>
+            <View flex justC='center' alignI='center' fD='column'>
+                <h3>Back to</h3>
+                <NavLink to='/'> Home Page </NavLink>
+            </View>
+        </View>
     )
 }
 
